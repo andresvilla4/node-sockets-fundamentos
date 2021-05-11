72e9290ca563c92eb57c52a10ceae5f727fbb624
andresvilla4 <andresmauriciovillacobgarrido@hotmail.com>
2021-05-11 09:53:09 -0500

    Configuracion del socket.io en el backend

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+const path = require('path');
+const publicPath = path.resolve(__dirname, '../public');
+const port = process.env.PORT || 3000;
+const app = express();
+const server = http.createServer(app);
+
+
+app.use(express.static(publicPath));
+
+// Comunicación del backend
+let io = socketIO(server);
+
+server.listen(port, (err) => {
+
+    if (err) throw new Error(err);
+
+    console.log(`Servidor corriendo en puerto ${ port }`);
+
+});
